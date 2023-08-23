ee1dd08b66371f8ebee6a56eadbd3901bd8d31dc
Lewis, Logan A <logan.lewis@pnnl.gov>
2023-08-23 14:32:31 -0700

    added most of requested changes, including adding more parameters for users

--- R/isoforma_pipeline.R
+++ R/isoforma_pipeline.R
@@ -1,45 +1,46 @@
 #' Calculates fragments, sums isotopes and charge states, generates the abundance matrix,
 #'     and calculates the proportions of each proteoform
-#' 
+#'
 #' @description A wrapper function to run fragments_per_ptm, sum_isotopes, abundance_matrix,
 #'    and calculate_proportions.
-#' 
+#'
 #' @param Sequences (character) A vector of valid ProForma sequences. Required.
-#' @param SummedSpectra (peak_data) A data.table with a "M/Z" and "Intensity" column, 
+#' @param SummedSpectra (peak_data) A data.table with a "M/Z" and "Intensity" column,
 #'     preferably calculated with the function sum_spectra. Required.
-#' @param PrecursorCharge (numeric) A single numeric to determine the Precursor 
+#' @param MaxFragmentCharge (numeric) A single numeric to determine the Precursor
 #'     Charge state (maximum charge) to consider fragments in the calculation. Required.
-#' @param ActivationMethod (character) A string to determine ions. "HCD", "CID", and 
-#'     "ETD" select b and y; a, b, and y; and c and z, respectively. Anything else 
+#' @param ActivationMethod (character) A string to determine ions. "HCD", "CID", and
+#'     "ETD" select b and y; a, b, and y; and c and z, respectively. Anything else
 #'     returns all 6 ions. Required.
 #' @param IonGroup (character) Must be an ion group to subset data down to (a-c or x-z). Required.
-#' @param CorrelationScore (numeric) A minimum Correlation Score to filter peaks with 
+#' @param CorrelationScore (numeric) A minimum Correlation Score to filter peaks with
 #'     at least 2 isotopes. Default is 0.7. Optional.
-#' @param PPMThreshold (numeric) A minimum PPM Threshold for matching calculated 
+#' @param PPMThreshold (numeric) A minimum PPM Threshold for matching calculated
 #'     and exprimental peaks. Default is 10. Optional.
-#' @param IsotopeAlgorithm (character) "isopat" uses the isopat package to calculate 
-#'     isotopes, while "Rdisop" uses the Rdisop package. Though more accurate, 
-#'     Rdisop has been known to crash on Windows computers when called iteratively 
+#' @param IsotopeAlgorithm (character) "isopat" uses the isopat package to calculate
+#'     isotopes, while "Rdisop" uses the Rdisop package. Though more accurate,
+#'     Rdisop has been known to crash on Windows computers when called iteratively
 #'     more than 1000 times. Default is Rdisop, though isopat is an alternative.
+#' @param MinimumAbundance (numeric) Minimum abundance for calculating isotopes. Default is 0.01.
 #' @param Message (logic) A TRUE/FALSE to indicate whether status messages should be printed. Default is FALSE.
 #' @param Top	(integer) The top N proportions to return, ranked from higest to lowest. Default is 8.
-#' 
-#' @returns (list) A list with 5 objects: matched peaks isoforma, summed isotopes isoforma, 
+#'
+#' @returns (list) A list with 5 objects: matched peaks isoforma, summed isotopes isoforma,
 #'     abundance matrix isoforma, the proportions dataframe, and a ggplot of the proportions
-#' 
+#'
 #' @examples
 #' \dontrun{
-#' 
-#' # Load 3 peaks to sum 
+#'
+#' # Load 3 peaks to sum
 #' PeakDataList <- list(
 #'  readRDS(system.file("extdata", "PeakData_1to1to1_1.RDS", package = "isoforma")),
 #'  readRDS(system.file("extdata", "PeakData_1to1to1_2.RDS", package = "isoforma")),
 #'  readRDS(system.file("extdata", "PeakData_1to1to1_3.RDS", package = "isoforma"))
 #' )
-#' 
-#' # Sum peaks 
+#'
+#' # Sum peaks
 #' PeakSum <- sum_ms2_spectra(PeakDataList = PeakDataList)
-#' 
+#'
 #' # Get proteoform strings
 #' MultipleMods <- pspecterlib::multiple_modifications(
 #'     Sequence = "LQIFVKTLTGKTITLEVEPSDTIENVKAKIQDKEGIPPDQQRLIFAGKQLEDGRTLSDYNIQKESTLHLVLRLRGG",
@@ -51,46 +52,48 @@
 #' IsoForma <- isoforma_pipeline(
 #'     Sequences = MultipleMods,
 #'     SummedSpectra = PeakSum,
-#'     PrecursorCharge = 11,
+#'     MaxFragmentCharge = 11,
 #'     ActivationMethod = "ETD",
 #'     IonGroup = "c",
 #'     CorrelationScore = 0,
-#'     IsotopeAlgorithm = "Rdisop", # If this step crashes, switch to "isopat" 
+#'     IsotopeAlgorithm = "Rdisop", # If this step crashes, switch to "isopat"
 #'     Messages = TRUE
 #' )
-#' 
+#'
 #' }
-#' 
+#'
 #'
 #' @export
 isoforma_pipeline <- function(Sequences,
                               SummedSpectra,
-                              PrecursorCharge,
+                              MaxFragmentCharge,
                               ActivationMethod,
                               IonGroup,
                               CorrelationScore = 0.7,
                               PPMThreshold = 10,
                               IsotopeAlgorithm = "Rdisop",
-                              Messages = FALSE, 
+                              MinimumAbundance = 0.01,
+                              Messages = FALSE,
                               Top = 8) {
 
   ##################
   ## RUN ISOFORMA ##
   ##################
 
-  # Steps 1 and 2, which are peak selection and peak summing, can be accomplished 
+  # Steps 1 and 2, which are peak selection and peak summing, can be accomplished
   # outside of this function using pull_scan_numbers() and sum_ms2_spectra().
 
   # 3. Calculate fragments per PTM
   FragmentsPerPTM <- fragments_per_ptm(Sequences = Sequences,
                                        SummedSpectra = SummedSpectra,
-                                       PrecursorCharge = PrecursorCharge, 
+                                       MaxFragmentCharge = MaxFragmentCharge,
                                        ActivationMethod = ActivationMethod,
-                                       CorrelationScore = CorrelationScore, 
-                                       PPMThreshold = PPMThreshold, 
-                                       IsotopeAlgorithm = IsotopeAlgorithm, 
+                                       CorrelationScore = CorrelationScore,
+                                       PPMThreshold = PPMThreshold,
+                                       IsotopeAlgorithm = IsotopeAlgorithm,
+                                       MinimumAbundance = MinimumAbundance,
                                        Messages = Messages)
-  
+
   # 4. Sum isotopes
   SummedIsotopes <- sum_isotopes(FragmentsPerPTM)
 
@@ -108,9 +111,9 @@ isoforma_pipeline <- function(Sequences,
   # Return all objects
   return(
     list(
-      FragmentsPerPTM, 
-      SummedIsotopes, 
-      AbundanceMatrix, 
+      FragmentsPerPTM,
+      SummedIsotopes,
+      AbundanceMatrix,
       Proportions[[1]],
       Proportions[[2]]
     )
--- R/pull_scan_numbers.R
+++ R/pull_scan_numbers.R
@@ -3,200 +3,201 @@ Glossary <- data.table::fread(
 )
 
 ## Define a function for matching peaks in isoforma
-.count_matches <- function(PeakData, SeqIsotopes, MS2Window) {
-  
+.count_matches <- function(PeakData, ppmMzTolerance, SeqIsotopes, MS2Window) {
+
   # Change peakdata class
   class(PeakData) <- c("data.table", "data.frame")
-  
+
   # Filter down to the MZ range
   FilterPeaks <- PeakData %>%
     dplyr::filter(`M/Z` >= min(SeqIsotopes$`M/Z`) & `M/Z` <= max(SeqIsotopes$`M/Z`))
-  
+
   if (nrow(FilterPeaks) == 0) {
     return(c("MS1 Matches" = 0, "MS1 Non-Matches" = 0,
              "MS1 Matches in MS2 Window" = 0))
   }
-  
+
   FilterPeaks <- FilterPeaks %>%
     dplyr::rename(`Experimental M/Z` = `M/Z`, `Experimental Intensity` = `Intensity`) %>%
     dplyr::mutate(`Experimental Abundance` = `Experimental Intensity` / max(`Experimental Intensity`) * 100)
-  
+
   # If nothing
   if (nrow(FilterPeaks) == 0) {return(c("Matched" = 0, "Not Matched" = nrow(SeqIsotopes)))}
-  
-  # Add PPM error 
-  SeqIsotopes$`M/Z Tolerance` <- SeqIsotopes$`M/Z` * (5 / 1e6)
-  
+
+  # Add PPM error
+  SeqIsotopes$`M/Z Tolerance` <- SeqIsotopes$`M/Z` * (ppmMzTolerance / 1e6)
+
   # Match closest within 5 ppm
   LeftIndex <- findInterval(SeqIsotopes$`M/Z`, FilterPeaks$`Experimental M/Z`, all.inside = TRUE)
   RightIndex <- LeftIndex + 1
   SeqIsotopes$`Left Difference` <- abs(FilterPeaks$`Experimental M/Z`[LeftIndex] - SeqIsotopes$`M/Z`)
   SeqIsotopes$`Right Difference` <- abs(FilterPeaks$`Experimental M/Z`[RightIndex] - SeqIsotopes$`M/Z`)
   SeqIsotopes$`Closest Index` <- LeftIndex
-  
+
   # Set closest index as right side one, if difference is smaller:
   RightIndexBest <- which(SeqIsotopes$`Right Difference` < SeqIsotopes$`Left Difference`)
   SeqIsotopes$`Closest Index`[RightIndexBest] <- SeqIsotopes$`Closest Index`[RightIndexBest] + 1
   SeqIsotopes$`M/Z Difference` <- abs(FilterPeaks$`Experimental M/Z`[SeqIsotopes$`Closest Index`] - SeqIsotopes$`M/Z`)
-  
+
   # Add PPM error
   SeqIsotopes$`PPM Error` <- SeqIsotopes$`M/Z Difference` / SeqIsotopes$`M/Z` * 1e6
-  
+
   # Count up the matches
   Matches <- SeqIsotopes %>% dplyr::filter(`PPM Error` <= 15)
   Count <- Matches %>% nrow()
-  
+
   # Count up matches in MS2 window
   CountMS2 <- sum(Matches$`M/Z` >= min(MS2Window) & Matches$`M/Z` <= max(MS2Window))
-  
+
   return(c("MS1 Matches" = Count, "MS1 Non-Matches" = nrow(SeqIsotopes) - Count,
            "MS1 Matches in MS2 Window" = CountMS2))
-  
+
 }
 
-#' Return a vector of scan numbers based on a Precursor Mass, mass tolerance, and 
+#' Return a vector of scan numbers based on a Precursor Mass, mass tolerance, and
 #'    isolation window filtering
-#' 
+#'
 #' @param Sequence (character) A valid amino acid sequence. Any non-traditional symbols are
 #'     removed and "M." is ignored. Required.
-#' @param Modification (character) An IsoForma modifications annotation written as 
-#'     "PTM,Residue(Positions)[Number of Modifications]". Required. 
-#' @param ScanMetadata (pspecterlib scan metadata object) An object of the scan_metadata class from pspecterlib 
+#' @param Modification (character) An IsoForma modifications annotation written as
+#'     "PTM,Residue(Positions)[Number of Modifications]". Required.
+#' @param ScanMetadata (pspecterlib scan metadata object) An object of the scan_metadata class from pspecterlib
 #'     get_scan_metadata. Required.
 #' @param MassWindow (numeric) A m/z window for acceptable precursor masses. Default is 5.
 #' @param MinMS1Matches (numeric) Minimum number of MS1 peaks from the isotopic distribution
-#'     of the full peptide sequence that fall within the MS2 window. Default is 3. 
+#'     of the full peptide sequence that fall within the MS2 window. Default is 3.
+#' @param ppmMzTolerance (numeric) Precursor ppm m/z tolerance.  Default is 5.
 #' @param ActivationMethod (character) The method used by the MS instrument to dissociate fragments.
 #'     Default is ETD.
 #' @param AsDataframe (logical) A boolean to indicate whether you would like the values used
 #'     to determine the ScanNumbers included or not. Default is FALSE.
-#'     
-#' @returns A data.frame or list of MS2 scan numbers to sum 
-#' 
-#' @examples 
+#'
+#' @returns A data.frame or list of MS2 scan numbers to sum
+#'
+#' @examples
 #' \dontrun{
-#' 
-#' # Generate the ScanMetadata object 
+#'
+#' # Generate the ScanMetadata object
 #' ScanMetadata <- pspecterlib::get_scan_metadata(
 #'     MSPath = "/Users/degn400/Desktop/IsoForma_Test/Sorghum-Histone0622162L11.mzML"
 #' )
-#' 
+#'
 #' # Run function
 #' pull_scan_numbers(Sequence = "M.SGRGKGGKGLGKGGAKRHRKVLRDNIQGITKPAIRRLARRGGVKRISGLIYEETRGVLKIFLENVIRDAVTYTEHARRKTVTAMDVVYALKRQGRTLYGFGG",
 #'                   Modification = "Acetyl,X(1*,5,8,12,16,20)[2]",
 #'                   ScanMetdata = ScanMetadata,
-#'                   PrecursorMass = 11355.38,
 #'                   RTStart = 30,
 #'                   RTEnd = 60)
-#' 
+#'
 #' }
 #' @export
 pull_scan_numbers <- function(Sequence,
                               Modification,
                               ScanMetadata,
                               RTStart,
-                              RTEnd, 
+                              RTEnd,
                               MassWindow = 10,
                               MinMS1Matches = 3,
-                              ActivationMethod = "ETD", 
+                              ppmMzTolerance = 5,
+                              ActivationMethod = "ETD",
                               AsDataframe = FALSE,
                               MinAbundance = 5) {
-  
+
   ##################
   ## CHECK INPUTS ##
   ##################
-  
+
   # Check that sequence is a character string first
   if (!is.character(Sequence)) {
     stop("Sequence must be a string.")
   }
-  
-  # Remove M. and any "." from the sequence 
-  Sequence <- Sequence %>% 
+
+  # Remove M. and any "." from the sequence
+  Sequence <- Sequence %>%
     gsub(pattern = "M.", replacement = "", fixed = TRUE) %>%
     gsub(pattern = ".", replacement = "", fixed = TRUE)
-  
+
   # Use pspecterlib's sequence check
   if (!pspecterlib::is_sequence(Sequence)) {
     stop("Sequence is not an acceptable peptide/protein sequence. See ?pspecterlib::is_sequence for more details.")
   }
-  
+
   # Check that Modification is a string
   if (!is.character(Modification)) {
     stop("Modification must be a string.")
   }
-  
+
   # Check that the modification is in the glossary
   PTM <- Modification %>% strsplit(",") %>% unlist() %>% head(1)
   if (!(PTM %in% Glossary$Modification)) {
     stop(paste("The modification", PTM, "is not in the backend glossary."))
   }
-  
+
   # Check that ScanMetadata is of the appropriate class
   if ("scan_metadata" %in% class(ScanMetadata) == FALSE) {
     stop("ScanMetadata must be a scan_metadata object from get_scan_metadata in the pspecterlib package.")
   }
-  
+
   # A check to ensure that each single mass value is correct
   .NotSingleNumeric <- function(x) {length(x) != 1 | !is.numeric(x)}
-  
-  
-  # Ensure the MassWindow is a single numeric value 
+
+
+  # Ensure the MassWindow is a single numeric value
   if (.NotSingleNumeric(MassWindow)) {
     stop("MassWindow must be a single numeric value.")
   }
-  
+
   # Ensure that both RT start and end are also single numeric values
   if (.NotSingleNumeric(RTStart)) {stop("RTStart must be a single numeric value.")}
   if (.NotSingleNumeric(RTEnd)) {stop("RTEnd must be a single numeric value.")}
-  
+
   ##############################
   ## CALCULATE PRECURSOR MASS ##
   ##############################
-  
-  # Make a function to get molecular formula 
+
+  # Make a function to get molecular formula
   get_mol_form <- function(PTM, freq) {
-    
+
     # Extract molecular formula
     MolForm <- Glossary[Glossary$Modification == PTM, 4:ncol(Glossary)] %>%
       dplyr::select(colnames(.)[!is.na(.)]) %>%
       paste0(colnames(.), ., collapse = "") %>%
       pspecterlib::as.molform()
-    
+
     # Multiply molecular formula
     MolForm <- pspecterlib::multiply_molforms(MolForm, freq)
-    
+
     return(MolForm)
 
   }
-  
-  # Get modifications and count 
+
+  # Get modifications and count
   TotalForm <- NULL
   for (x in unlist(strsplit(Modification, ";"))) {
     Mod <- strsplit(x, ",") %>% unlist() %>% head(1)
-    Count <- strsplit(x, "[", fixed = T) %>% unlist() %>% 
+    Count <- strsplit(x, "[", fixed = T) %>% unlist() %>%
         tail(1) %>% gsub(pattern = "]", replacement = "", fixed = T) %>% as.numeric()
     if (is.null(TotalForm)) {TotalForm <- get_mol_form(Mod, Count)} else {
       TotalForm <- pspecterlib::add_molforms(TotalForm, get_mol_form(Mod, Count), CapNegatives = FALSE)
     }
   }
-  
-  # Now get the total molecular formula 
+
+  # Now get the total molecular formula
   MolForm <- pspecterlib::add_molforms(pspecterlib::get_aa_molform(Sequence), TotalForm)
-  
-  # Get full list of isotoping information for the molecular formula 
+
+  # Get full list of isotoping information for the molecular formula
   IsotopingData <- pspecterlib::calculate_iso_profile(MolForm, min_abundance = MinAbundance, 0.001)
-  
+
   # Get the exact mass
   exactmass <- pspecterlib::get_mw(MolForm)
-  
+
   #############################################################################
   ## DETERMINE SCAN NUMBERS WITHIN M/Z AND RT WINDOWS WITH ACTIVATION METHOD ##
   #############################################################################
-  
-  # Rewrite class of scan metadata 
+
+  # Rewrite class of scan metadata
   class(ScanMetadata) <- c("data.table", "data.frame")
-  
+
   # Pull MS2 scan numbers with a mass and retention time within the designated window
   MS2ScanNumbers <- ScanMetadata %>%
     dplyr::filter(`MS Level` == 2) %>%
@@ -204,81 +205,81 @@ pull_scan_numbers <- function(Sequence,
       "Precursor Charge" = ifelse(`Precursor Charge` == 0, 1, `Precursor Charge`),
       "Precursor Mass" = `Precursor M/Z` * `Precursor Charge` - (1.00727647 * `Precursor Charge`)
     ) %>%
-    dplyr::select(c(`Scan Number`, `Precursor Mass`, `Retention Time`, `Activation Method`, 
+    dplyr::select(c(`Scan Number`, `Precursor Mass`, `Retention Time`, `Activation Method`,
              `Precursor M/Z`, `Precursor Charge`, `Precursor Scan`)) %>%
-    dplyr::filter(`Precursor Mass` >= exactmass - MassWindow & 
+    dplyr::filter(`Precursor Mass` >= exactmass - MassWindow &
            `Precursor Mass` <= exactmass + MassWindow &
            `Retention Time` >= RTStart,
            `Retention Time` <= RTEnd,
            `Activation Method` == "ETD")
-  
-  # If no MS2ScanNumbers, return error 
+
+  # If no MS2ScanNumbers, return error
   if (nrow(MS2ScanNumbers) == 0) {
     message("No MS2 Scan Numbers detected. Please extend retention time and mass windows.")
     return(NULL)
   }
-  
+
   ##################################
   ## COUNT MATCHES WITHIN WINDOWS ##
   ##################################
-  
+
   # Get header information
   Header <- mzR::header(attr(ScanMetadata, "pspecter")$mzRpwiz)
-  
-  # Count up matches 
+
+  # Count up matches
   CountMatches <- do.call(rbind, lapply(1:nrow(MS2ScanNumbers), function(row) {
-      
+
     # Get the MS2 data
     MS2ScanNumber <- MS2ScanNumbers[row, "Scan Number"] %>% unlist()
     PreCharge <- ScanMetadata[ScanMetadata$`Scan Number` == MS2ScanNumber, "Precursor Charge"] %>% unlist()
     SeqIsotopes <- IsotopingData
     colnames(SeqIsotopes)[1] <- c("M/Z")
-    
+
     SeqIsotopes$`M/Z` <- SeqIsotopes$`M/Z` / PreCharge  + 1.007276
-    
-    # Pull Header data 
+
+    # Pull Header data
     IsolationMZ <- Header %>% dplyr::filter(acquisitionNum == MS2ScanNumber) %>% dplyr::select(isolationWindowTargetMZ) %>% unlist()
-    IsolationWindow <- c(IsolationMZ - Header[Header$acquisitionNum == MS2ScanNumber, "isolationWindowLowerOffset"], 
+    IsolationWindow <- c(IsolationMZ - Header[Header$acquisitionNum == MS2ScanNumber, "isolationWindowLowerOffset"],
                          IsolationMZ + Header[Header$acquisitionNum == MS2ScanNumber, "isolationWindowUpperOffset"])
-    
+
     # Get MS1 data
     MS1ScanNumber <- MS2ScanNumbers[row, "Precursor Scan"] %>% unlist()
     class(ScanMetadata) <- c(class(ScanMetadata), "scan_metadata")
     MS1Peaks <- pspecterlib::get_peak_data(ScanMetadata, MS1ScanNumber)
-    MS1_isomatch <- .count_matches(MS1Peaks, SeqIsotopes, IsolationWindow)
-    
+    MS1_isomatch <- .count_matches(MS1Peaks, ppmMzTolerance, SeqIsotopes, IsolationWindow)
+
     return(
-      c(MS2ScanNumber, 
+      c(MS2ScanNumber,
         PreCharge,
         "MS1 Matches" = MS1_isomatch[["MS1 Matches"]],
         "MS1 Non-Matches" = MS1_isomatch[["MS1 Non-Matches"]],
         "MS1 Matches in MS2 Window" = MS1_isomatch[["MS1 Matches in MS2 Window"]]
       )
     )
-    
+
   })) %>% data.table::data.table()
-  
+
   # Add to MS2 scan numbers
   MS2ScanNumbers[,c("MS1 Matches", "MS1 Non-Matches", "MS1 Matches in MS2 Window")] <- CountMatches[,c(3:5)]
-  
+
   # Filter down to proportions
   MS2ScanNumbers <- MS2ScanNumbers %>%
     dplyr::mutate(
-      MS1Total = `MS1 Matches` + `MS1 Non-Matches`, 
+      MS1Total = `MS1 Matches` + `MS1 Non-Matches`,
       MS2Total = max(`MS1 Matches in MS2 Window`),
       Use = `MS1 Matches in MS2 Window` > MinMS1Matches &
         `MS1 Matches` >= floor((MS2ScanNumbers$`MS1 Matches`[1] + MS2ScanNumbers$`MS1 Non-Matches`[1]) * 0.4)
     ) %>%
     dplyr::select(-c(MS1Total, MS2Total))
-  
+
   ####################
   ## RETURN RESULTS ##
   ####################
-  
+
   if (AsDataframe) {
-    
+
     return(list(MS2ScanNumbers, exactmass))
-    
+
   } else {return( MS2ScanNumbers[MS2ScanNumbers$Use, "Scan Number"])}
-  
+
 }
